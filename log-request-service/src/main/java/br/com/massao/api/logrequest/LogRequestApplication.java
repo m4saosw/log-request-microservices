ec08efff4cdcffc0b33d954802206bab21205983
Massao <lmassao@yahoo.com.br>
2021-07-17 23:40:39 -0300

    Cria a infraestrutura inicial do projeto

---
+++ log-request-service/src/main/java/br/com/massao/api/logrequest/LogRequestApplication.java
@@ -0,0 +1,13 @@
+package br.com.massao.api.logrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogRequestApplication.class, args);
+    }
+
+}
---
+++ log-request-service/src/test/java/br/com/massao/api/logrequest/LogRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.massao.api.logrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
