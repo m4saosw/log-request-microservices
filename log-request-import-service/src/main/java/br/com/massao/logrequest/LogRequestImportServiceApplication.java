e4967c27732687590463b14c34cf209021691e81
Massao <lmassao@yahoo.com.br>
2021-07-25 16:14:07 -0300

    Creates the initial structure of the project

---
+++ log-request-import-service/src/main/java/br/com/massao/logrequest/LogRequestImportServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.massao.logrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRequestImportServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogRequestImportServiceApplication.class, args);
+    }
+
+}
---
+++ log-request-import-service/src/test/java/br/com/massao/logrequest/LogRequestImportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.massao.logrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRequestImportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
