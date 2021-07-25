ac3ff2296719424e768625639c5f92ab03762e1a
Massao <lmassao@yahoo.com.br>
2021-07-25 20:48:26 -0300

    Refactoring of class names

--- log-request-service/src/main/java/br/com/massao/logrequest/LogRequestServiceApplication.java
+++ log-request-service/src/main/java/br/com/massao/logrequest/LogRequestServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogRequestApplication {
+public class LogRequestServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LogRequestApplication.class, args);
+        SpringApplication.run(LogRequestServiceApplication.class, args);
     }
 
 }
--- log-request-service/src/test/java/br/com/massao/logrequest/LogRequestServiceApplicationTests.java
+++ log-request-service/src/test/java/br/com/massao/logrequest/LogRequestServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LogRequestApplicationTests {
+class LogRequestServiceApplicationTests {
 
     @Test
     void contextLoads() {
