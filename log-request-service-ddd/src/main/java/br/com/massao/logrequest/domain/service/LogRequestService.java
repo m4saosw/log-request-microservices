154a9708a0b45e5cddcbeebcf2e83c1a8d1e87d4
Massao <lmassao@yahoo.com.br>
2021-08-22 19:35:33 -0300

    Corrige descrição do serviço de dominio para o tipo Port

--- log-request-service-ddd/src/main/java/br/com/massao/logrequest/domain/service/LogRequestService.java
+++ log-request-service-ddd/src/main/java/br/com/massao/logrequest/domain/service/LogRequestService.java
@@ -2,15 +2,14 @@ package br.com.massao.logrequest.domain.service;
 
 import br.com.massao.logrequest.domain.DomainLogRequest;
 import br.com.massao.logrequest.domain.NotFoundException;
-import br.com.massao.logrequest.infrastructure.model.LogRequestModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 /**
- *  Tambem chamado de Adapter na hexagonal architecture.
- *  Representa um Adaptador para uma Porta
+ * Porta do Servico de Dominio
+ * Port na hexagonal architecture.
  */
 @Service
 public interface LogRequestService {
@@ -37,7 +36,7 @@ public interface LogRequestService {
     /**
      * Save a new log
      *
-     * @param model
+     * @param domain
      * @return
      */
     DomainLogRequest save(DomainLogRequest domain);
